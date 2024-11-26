25750ba233ed4644e1bb4a524debcddfad7064a9
muhammadFaizan <faizanvkey@gmail.com>
2024-11-26 20:57:26 +0500

    implement get/set commands

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import socket  # noqa: F401
 import threading
 from app.resp_utils import encode_resp, decode_resp
 
+storage = {}
+
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     
@@ -33,9 +35,13 @@ def connect(connection: socket.socket):
                     case ['ECHO', message]:
                         response = encode_resp(message)
                     case ['GET', key]:
-                        response = encode_resp(f'mock_value_for_{key}')
+                        if key not in storage:
+                            response = encode_resp(None)
+                        else:
+                            response = encode_resp(storage[key])
                     case ['SET', key, value]:
-                        response = encode_resp(f'mock_set_{key}_{value}')
+                        storage[key] = value
+                        response = encode_resp('OK')
                     case ['DEL', key]:
                         response = encode_resp(f'mock_del_{key}')
                     case _:
