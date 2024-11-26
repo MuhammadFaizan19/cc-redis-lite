c906c8f688027e78de8305660cbb66c227bff8da
muhammadFaizan <faizanvkey@gmail.com>
2024-11-26 18:22:08 +0500

    handle multiple ping commands

--- app/main.py
+++ app/main.py
@@ -1,15 +1,35 @@
 import socket  # noqa: F401
+import threading
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
-    connection.sendall(b'+PONG\r\n')
+    
+    while True:
+        connection: socket.socket
+        address: tuple[str, str]
+        connection, address = server_socket.accept()
+
+        thread: threading.Thread = threading.Thread(target=connect, args=(connection,))
+        thread.start()
+
+
+def connect(connection: socket.socket):
+    with connection:
+        connected: bool = True
+        while connected:
+            command: str = connection.recv(8000).decode()
+            print(f'recieved - {command}')
+            connected = bool(command)
+
+            response: str
+            match command:
+                case '*1\r\n$4\r\nPING\r\n':
+                    response = '+PONG\r\n'
+            
+            connection.sendall(response.encode())
+            print(f'sent - {response}')
+
 
 if __name__ == "__main__":
     main()
